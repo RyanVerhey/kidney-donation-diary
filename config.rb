7c7e9bf77f2ec691e85c680d48f5acd6802a3946
RyanVerhey <ryan90v@gmail.com>
2019-11-03 17:24:11 -0500

    Adding some elements

--- config.rb
+++ config.rb
@@ -19,16 +19,17 @@ page '/*.txt', layout: false
 ###
 # Helpers
 ###
+activate :directory_indexes
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "{year}{month}{day}-{title}.html"
   # Matcher for blog source files
   blog.sources = "articles/{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
+  blog.layout = "layouts/layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
