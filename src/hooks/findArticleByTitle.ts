5dc6b0af11625a02d6a8b3adeff4987cdcceb8b8
Ryan Verhey <ryan@verhey.email>
2021-06-06 17:45:38 -0400

    Removing logging

--- src/hooks/findArticleByTitle.ts
+++ src/hooks/findArticleByTitle.ts
@@ -3,7 +3,6 @@ import allArticles from './allArticles';
 
 export default function findArticleByTitle(title: string): iArticle {
   return allArticles().filter(article => {
-    console.log(article.frontmatter.title.toLowerCase(), title.toLowerCase(), article.frontmatter.title.toLowerCase() === title.toLowerCase())
     return article.frontmatter.title.toLowerCase() === title.toLowerCase();
   })[0]
 }
