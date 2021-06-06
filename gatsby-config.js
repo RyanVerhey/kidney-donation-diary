23e298b8cff7885464b338effc7cee2c8ffa1368
Ryan Verhey <ryan@verhey.email>
2021-06-06 17:45:27 -0400

    Adding RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,75 @@ const path = require('path')
 module.exports = {
   siteMetadata: {
     title: "kidney-donation-diary",
+    siteUrl: "https://kidneydonationdiary.com",
+    description: "Hi, I'm Ryan, and I gave one of my kidneys away. Please join me on my organ donation journey.",
+    blogAuthor: "Ryan Verhey"
   },
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-feed",
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                blogAuthor
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.articles.map(article => {
+                return {
+                  title: article.frontmatter.title,
+                  date: article.frontmatter.date,
+                  author: article.frontmatter.author || site.siteMetadata.blogAuthor,
+                  description: article.excerpt,
+                  url: site.siteMetadata.siteUrl + "/" + article.slug,
+                  guid: site.siteMetadata.siteUrl + "/" + article.slug,
+                }
+              });
+            },
+            query: `
+              {
+                allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+                  articles:nodes {
+                    id
+                    slug
+                    excerpt(pruneLength: 103)
+                    frontmatter {
+                      date
+                      title
+                      author
+                    }
+                  }
+                }
+              }
+            `,
+            output: 'feed.xml',
+            title: 'Kidney Donation Diary RSS Feed',
+          }
+        ]
+      }
+    },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
         name: "Kidney Donation Diary",
         short_name: "Kidney Diary",
         start_url: '/',
+        theme_color: "#800020",
         icon: "src/images/favicon.png",
       },
     },
@@ -23,8 +81,6 @@ module.exports = {
         extensions: [`.mdx`, `.md`, `.markdown`],
       }
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
